ce71eb8d5d7ae5b2994f3e02e72494e84d8a2dc2
Laurens Valk <laurens@pybricks.com>
2021-01-11 13:48:39 +0100

    ev3: copy mailbox server example
    from https://github.com/pybricks/pybricks-api/tree/master/examples/ev3/bluetooth_server

--- ev3/main.py
+++ ev3/main.py
@@ -1,20 +1,17 @@
 #!/usr/bin/env pybricks-micropython
-from pybricks.hubs import EV3Brick
-from pybricks.ev3devices import (Motor, TouchSensor, ColorSensor,
-                                 InfraredSensor, UltrasonicSensor, GyroSensor)
-from pybricks.parameters import Port, Stop, Direction, Button, Color
-from pybricks.tools import wait, StopWatch, DataLog
-from pybricks.robotics import DriveBase
-from pybricks.media.ev3dev import SoundFile, ImageFile
 
+from pybricks.messaging import BluetoothMailboxServer, TextMailbox
 
-# This program requires LEGO EV3 MicroPython v2.0 or higher.
-# Click "Open user guide" on the EV3 extension tab for more information.
+server = BluetoothMailboxServer()
+mbox = TextMailbox('greeting', server)
 
+# The server must be started before the client!
+print('waiting for connection...')
+server.wait_for_connection()
+print('connected!')
 
-# Create your objects here.
-ev3 = EV3Brick()
-
-
-# Write your program here.
-ev3.speaker.beep()
+# In this program, the server waits for the client to send the first message
+# and then sends a reply.
+mbox.wait()
+print(mbox.read())
+mbox.send('hello to you!')

16a3fbbf051a3e5285f8e8867017b780e5c6ee09
Laurens Valk <laurens@pybricks.com>
2021-01-11 13:49:09 +0100

    main: copy mailbox pc client example
    from: https://github.com/pybricks/pybricks-api/tree/master/examples/ev3/bluetooth_pc

--- main.py
+++ main.py
@@ -1,2 +1,18 @@
-import os
-print(os.environ['VIRTUAL_ENV'])
+#!/usr/bin/env python3
+from mailbox.messaging import BluetoothMailboxClient, TextMailbox
+
+# This is the address of the server EV3 we are connecting to.
+SERVER = '24:71:89:4A:02:E2'
+
+client = BluetoothMailboxClient()
+mbox = TextMailbox('greeting', client)
+
+print('establishing connection...')
+client.connect(SERVER)
+print('connected!')
+
+# In this program, the client sends the first message and then waits for the
+# server to reply.
+mbox.send('hello!')
+mbox.wait()
+print(mbox.read())

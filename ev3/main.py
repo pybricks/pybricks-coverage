706d29371a0fc4fb79119c40c074ff94dfeb7abb
Laurens Valk <laurens@pybricks.com>
2021-03-12 12:49:10 +0100

    ev3: keep accepting connections and commands
    Allow the EV3 to just keep running while we restart the test scripts.

--- ev3/main.py
+++ ev3/main.py
@@ -1,34 +1,44 @@
 #!/usr/bin/env pybricks-micropython
 
 from pybricks.messaging import BluetoothMailboxServer, TextMailbox
+from pybricks.tools import wait
 
 from monitor import SpikeMonitor
 
 # Create monitor object and initialize mechanisms
 spike = SpikeMonitor()
 
-# Wait for incoming bluetooth connection
+# Start server
 server = BluetoothMailboxServer()
-mbox = TextMailbox('command', server)
-print('waiting for connection...')
-server.wait_for_connection()
-print('connected!')
 
+# Keep accepting connections and commands from PC script.
 while True:
-    # Wait for new instruction
-    mbox.wait()
-    command = mbox.read()
-    print(command)
 
-    # Execute command
-    if command == 'activate_dfu':
-        spike.activate_dfu()
-    elif command == 'remove_usb':
-        spike.insert_usb(False)
-    elif command == 'shutdown':
-        spike.shutdown()
-    elif command == 'stop':
-        break
+    # Wait for incoming bluetooth connection
 
-    # Say we are done with this command
-    mbox.send(command)
+    mbox = TextMailbox('command', server)
+    print('waiting for connection...')
+    server.wait_for_connection()
+    print('connected!')
+
+    while True:
+        # Wait for new instruction
+        mbox.wait()
+        command = mbox.read()
+        print(command)
+
+        # Execute command
+        if command == 'power_on':
+            spike.click_center()
+        if command == 'activate_dfu':
+            spike.activate_dfu()
+        elif command == 'remove_usb':
+            spike.insert_usb(False)
+        elif command == 'shutdown':
+            spike.shutdown()
+
+        # Say we are done with this command
+        mbox.send(command)
+
+        if command == 'stop':
+            break
